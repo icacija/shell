d765243319d6f8988c586fd1d99eb2e7bc344b78
Ivan Cacija <ivan.cacija@gmail.com>
2022-03-07 13:45:45 +0100

    Update jetbackup-install-cpanel.sh

--- jetbackup-install-cpanel.sh
+++ jetbackup-install-cpanel.sh
@@ -1,1 +1,7 @@
-#!/bin.sh
+# /bin.sh
+# From https://docs.jetbackup.com/v5.1/adminpanel/generalInformation.html
+yum install http://repo.jetlicense.com/centOS/jetapps-repo-latest.rpm
+yum clean all --enablerepo=jetapps*
+yum install jetapps --disablerepo=* --enablerepo=jetapps
+jetapps --install jetbackup5-cpanel stable
+echo "All done";
