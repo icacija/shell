3dc5d983807df3d8def019d8cab51810098b9cc2
Ivan Cacija <ivan.cacija@gmail.com>
2022-03-08 13:52:08 +0100

    Create cpanel-cloudns.sh

---
+++ cpanel-cloudns.sh
@@ -0,0 +1,9 @@
+# /bin/bash
+echo -e "Install CSE"
+wget https://raw.githubusercontent.com/icacija/shell/main/cpanel-cse.sh
+echo -e "\Done"
+echo -e "\Create fastserver"
+mkdir /etc/fastserver
+cd /etc/fastserver
+wget https://raw.githubusercontent.com/ClouDNS/cloudns-api-bulk-updates/master/cpanel-slave-zones-add/cpanel-slave-zones-add.php
+echo -e "Potrebno je rucno urediti ostalo"
