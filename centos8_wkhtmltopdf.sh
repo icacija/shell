ada47229662ff5ab84504968c8b0e14b09a140c2
Ivan Cacija <ivan.cacija@gmail.com>
2022-03-13 12:57:47 +0100

    Update and rename centos8_wkhtmltopdf to centos8_wkhtmltopdf.sh

--- centos8_wkhtmltopdf.sh
+++ centos8_wkhtmltopdf.sh
@@ -2,5 +2,9 @@
 yum install xorg-x11-fonts-Type1 xorg-x11-fonts-75dpi -y
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox-0.12.6-1.centos8.x86_64.rpm
 rpm -i wkhtmltox-0.12.6-1.centos8.x86_64.rpm
-echo -e "All done"
+echo -e "Installation done \ Testing:"
 wkhtmltopdf https://google.hr google.pdf
+echo -e "All done - cleaning \ "
+rm -rf wkhtmltox-0.12.6-1.centos8.x86_64.rpm
+rm -rf centos8_wkhtmltopdf.sh
+
