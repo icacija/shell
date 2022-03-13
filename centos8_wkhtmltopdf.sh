b7448dc7fc302a40488a43e50c79e07cba7d5166
Ivan Cacija <ivan.cacija@gmail.com>
2022-03-13 13:00:46 +0100

    Update centos8_wkhtmltopdf.sh

--- centos8_wkhtmltopdf.sh
+++ centos8_wkhtmltopdf.sh
@@ -2,9 +2,9 @@
 yum install xorg-x11-fonts-Type1 xorg-x11-fonts-75dpi -y
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox-0.12.6-1.centos8.x86_64.rpm
 rpm -i wkhtmltox-0.12.6-1.centos8.x86_64.rpm
-echo -e "Installation done \ Testing:"
+echo -e "Installation done \nTesting:"
 wkhtmltopdf https://google.hr google.pdf
-echo -e "All done - cleaning \ "
+echo -e "All done - cleaning \nIvan je najbolji "
 rm -rf wkhtmltox-0.12.6-1.centos8.x86_64.rpm
 rm -rf centos8_wkhtmltopdf.sh
 
