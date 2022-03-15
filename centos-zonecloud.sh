df9b18b564dd514837f9a9409f9cdb7ee9b1ab8a
Ivan Cacija <ivan.cacija@gmail.com>
2022-03-15 13:01:51 +0100

    Update centos-zonecloud.sh

--- centos-zonecloud.sh
+++ centos-zonecloud.sh
@@ -1,4 +1,6 @@
 # /bin/bash
+wget https://raw.githubusercontent.com/icacija/shell/main/cpanel-cse.sh
+bash cpanel-cse.sh
 wget http://repo.nixpal.com/el8/nixpal-el8-1.1-1.el8.x86_64.rpm
 yum localinstall nixpal-el8-1.1-1.el8.x86_64.rpm
 yum clean all
