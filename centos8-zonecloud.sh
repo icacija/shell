2c8b99c46e2348f90cdd5dc70f69107d4d3f4e70
Ivan Cacija <ivan.cacija@gmail.com>
2022-03-15 14:06:42 +0100

    Update and rename centos-zonecloud.sh to centos8-zonecloud.sh

--- centos8-zonecloud.sh
+++ centos8-zonecloud.sh
@@ -4,5 +4,5 @@ bash cpanel-cse.sh
 wget http://repo.nixpal.com/el8/nixpal-el8-1.1-1.el8.x86_64.rpm
 yum localinstall nixpal-el8-1.1-1.el8.x86_64.rpm
 yum clean all
-yum install zcloudagent
+yum install zcloudagent -y
 echo -e "CSE i Zonecloud instalirani \nPotrebna daljna konfiguracija"
