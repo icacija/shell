bd9f0f86eef494a5cff0121a9c2f678764753bfd
Ivan Cacija <ivan.cacija@gmail.com>
2022-03-16 21:56:54 +0100

    Update centos7-zonecloud.sh

--- centos7-zonecloud.sh
+++ centos7-zonecloud.sh
@@ -1,5 +1,9 @@
 # /bin/bash
+wget https://raw.githubusercontent.com/icacija/shell/main/cpanel-cse.sh
+bash cpanel-cse.sh
 wget http://repo.nixpal.com/el7/nixpal-el7-1.1-1.el7.x86_64.rpm
 yum -y localinstall nixpal-el7-1.1-1.el7.x86_64.rpm 
 yum clean all
-yum install zcloudagent
+yum -y install zcloudagent 
+echo -e "CSE i Zonecloud instalirani \nPotrebna daljna konfiguracija"
+rm -rf centos7-zonecloud.sh
