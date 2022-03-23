c556e354c456c9aaa02362f4be7224c4816293c3
Ivan Cacija <ivan.cacija@gmail.com>
2022-03-23 12:46:04 +0100

    Create cpanel-postinstall.sh

---
+++ cpanel-postinstall.sh
@@ -0,0 +1,30 @@
+# /bin/bash
+
+echo -e "CSF Install"
+
+cd /usr/src
+rm -fv csf.tgz
+wget https://download.configserver.com/csf.tgz
+tar -xzf csf.tgz
+cd csf
+sh install.sh
+
+echo -e "CMQ Install"
+cd /usr/src
+rm -fv /usr/src/cmq.tgz
+wget https://download.configserver.com/cmq.tgz
+tar -xzf cmq.tgz
+cd cmq
+sh install.sh
+rm -Rfv /usr/src/cmq*
+
+echo -e "CSE Install"
+cd /usr/src
+rm -fv /usr/src/cse.tgz
+wget https://download.configserver.com/cse.tgz
+tar -xzf cse.tgz
+cd cse
+sh install.sh
+rm -Rfv /usr/src/cse*
+
+rm -rf cpanel-postinstall.sh
