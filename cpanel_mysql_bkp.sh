1fc25c5d104ac884a7d0495fd32fe65736f906d4
Ivan Cacija <ivan.cacija@gmail.com>
2022-04-11 18:47:22 +0200

    Update cpanel_mysql_bkp.sh

--- cpanel_mysql_bkp.sh
+++ cpanel_mysql_bkp.sh
@@ -1,7 +1,14 @@
 # /bin/bash
-cd /backup/db
-mkdir $(date +"%Y-%m-%d")
-cd $(date +"%Y-%m-%d")
-for I in $(mysql -e 'show databases' -s --skip-column-names); do mysqldump2 $I | gzip > "$I.sql.gz"; done
-echo -e "Cleaning old bkps"
-find /backup/db/* -mtime +2 -exec rm -rf {} \;
+dir="/backup/db"
+if [ ! -d $dir ]
+then
+    mkdir $dir
+else
+    cd $dir
+    mkdir $(date +"%Y-%m-%d")
+    cd $(date +"%Y-%m-%d")
+    for I in $(mysql -e 'show databases' -s --skip-column-names); do mysqldump2 $I | gzip > "$I.sql.gz"; done
+    cd $dir
+    echo -e "Cleaning old bkps"
+    find $dir/* -mtime +2 -exec rm -rf {} \;
+fi
