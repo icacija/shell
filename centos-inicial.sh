1f3670a09e3b7916cf1a18e29fb41223e742352d
Ivan Cacija <ivan.cacija@gmail.com>
2022-06-27 16:07:55 +0200

    Update centos-inicial.sh
    bash dodan na kraj

--- centos-inicial.sh
+++ centos-inicial.sh
@@ -12,8 +12,6 @@ bashrc='/etc/bashrc'
 script64='IyBDb2xvcnMgaW4gVGVybWluYWwKaWYgWyAkVVNFUiA9IHJvb3QgXTsgdGhlbgogICAgICAgIFBTMT0nXFtcMDMzWzE7MzFtXF1bXHVAXGggXFddXCRcW1wwMzNbMG1cXSAnCmVsc2UKICAgICAgICBQUzE9J1xbXDAzM1swMTszMm1cXVx1QFxoXFtcMDMzWzAwbVxdIFxbXDAzM1swMTszNG1cXVxXXFtcMDMzWzAwbVxdXFtcMDMzWzE7MzJtXF1cJFxbXDAzM1ttXF0gJwpmaQo='
 echo $script64 | base64 -d >> $bashrc
 
-bash
-
 echo -e "Change timezone to Europe/Zagreb"
 timedatectl set-timezone Europe/Zagreb
 
@@ -22,3 +20,5 @@ echo -e "Done for now \nPlease reboot"
 
 echo -e "clean-up"
 rm -rf centos-inicial.sh
+
+bash
