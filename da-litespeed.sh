6170bd46b2445b0a8bcfb5205a77d5805f5578b4
Ivan Cacija <ivan.cacija@gmail.com>
2022-07-27 17:24:10 +0200

    Create da-litespeed.sh

---
+++ da-litespeed.sh
@@ -0,0 +1,10 @@
+# /bin/bash
+cd /usr/local/directadmin/custombuild
+./build update
+./build set webserver litespeed
+./build set php1_mode lsphp
+./build set php2_mode lsphp
+./build set php3_mode lsphp
+./build set php4_mode lsphp
+./build litespeed
+./build php n
