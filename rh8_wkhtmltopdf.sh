dad61284d79e4028ec6e837f08c7ae2ae445564b
Ivan Cacija <ivan.cacija@gmail.com>
2022-09-02 11:22:22 +0200

    Update and rename 8_wkhtmltopdf.sh to rh8_wkhtmltopdf.sh

--- rh8_wkhtmltopdf.sh
+++ rh8_wkhtmltopdf.sh
@@ -1,10 +1,18 @@
 # /bin/bash
+
+#Get script path
+SCRIPT=$(readlink -f "$0")
+
 yum install xorg-x11-fonts-Type1 xorg-x11-fonts-75dpi -y
+
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox-0.12.6-1.centos8.x86_64.rpm
+
 rpm -i wkhtmltox-0.12.6-1.centos8.x86_64.rpm
 echo -e "Installation done \nTesting:"
 wkhtmltopdf https://google.hr google.pdf
 echo -e "All done - cleaning \nIvan je najbolji "
-rm -rf wkhtmltox-0.12.6-1.centos8.x86_64.rpm
-rm -rf centos8_wkhtmltopdf.sh
 
+rm -rf wkhtmltox-0.12.6-1.centos8.x86_64.rpm
+
+echo -e "Clean up"
+rm -rf "$SCRIPT"
