217a8b4c8c78091f7d525d626d17babc6318b3c6
Ivan Cacija <ivan.cacija@gmail.com>
2022-09-23 15:27:27 +0200

    Create rh7-inicial.sh

---
+++ rh7-inicial.sh
@@ -0,0 +1,38 @@
+# /bin/bash
+
+# /bin/bash
+
+#Get script path
+SCRIPT=$(readlink -f "$0")
+
+echo -e "Disable selinux"
+sudo sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config
+sudo sestatus
+
+echo -e "Disable firewall"
+systemctl stop firewalld
+systemctl disable firewalld
+
+echo -e "Change SSH port to 9001"
+echo "Port 9001" >> /etc/ssh/sshd_config
+
+echo -e "Change root color"
+bashrc='/etc/bashrc'
+script64='IyBDb2xvcnMgaW4gVGVybWluYWwKaWYgWyAkVVNFUiA9IHJvb3QgXTsgdGhlbgogICAgICAgIFBTMT0nXFtcMDMzWzE7MzFtXF1bXHVAXGggXFddXCRcW1wwMzNbMG1cXSAnCmVsc2UKICAgICAgICBQUzE9J1xbXDAzM1swMTszMm1cXVx1QFxoXFtcMDMzWzAwbVxdIFxbXDAzM1swMTszNG1cXVxXXFtcMDMzWzAwbVxdXFtcMDMzWzE7MzJtXF1cJFxbXDAzM1ttXF0gJwpmaQo='
+echo $script64 | base64 -d >> $bashrc
+
+echo -e "Change timezone to Europe/Zagreb"
+timedatectl set-timezone Europe/Zagreb
+
+echo -e "EPEL"
+yum install epel-release -y
+
+echo -e "HTOP & VNSTAT & FTP & NANO & SCREEN" 
+yum install htop vnstat ftp nano screen wget -y
+
+echo -e "Done for now \nPlease reboot"
+
+echo -e "Clean up"
+rm -rf "$SCRIPT"
+
+bash
