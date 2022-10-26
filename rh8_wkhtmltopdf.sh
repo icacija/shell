8ae9f24e163d1c302aace4f3e81769e9596f7b2b
Ivan Cacija <ivan.cacija@gmail.com>
2022-10-26 16:22:58 +0200

    dodan libXrender na yum
    falio libXrender prilikom instalacije

--- rh8_wkhtmltopdf.sh
+++ rh8_wkhtmltopdf.sh
@@ -3,7 +3,7 @@
 #Get script path
 SCRIPT=$(readlink -f "$0")
 
-yum install xorg-x11-fonts-Type1 xorg-x11-fonts-75dpi -y
+yum install xorg-x11-fonts-Type1 xorg-x11-fonts-75dpi libXrender -y
 
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox-0.12.6-1.centos8.x86_64.rpm
 
