31413aca559e8e66cfaa06b4c6cb169c51692b7e
Ivan Cacija <ivan.cacija@gmail.com>
2022-11-09 21:16:12 +0100

    Update na php verzijama
    ./build set php2_mode php-fpm
    ./build set php3_mode php-fpm
    ./build set php4_mode php-fpm

--- da-ngnix-apache.sh
+++ da-ngnix-apache.sh
@@ -3,6 +3,9 @@ cd /usr/local/directadmin/custombuild
 ./build update_da
 ./build set webserver nginx_apache
 ./build set php1_mode php-fpm
+./build set php2_mode php-fpm
+./build set php3_mode php-fpm
+./build set php4_mode php-fpm
 ./build nginx_apache
 ./build php n
 ./build rewrite_confs
