ec25cd694fc0e56e5ce6dd30ee564939caed3e69
Ivan Cacija <ivan.cacija@gmail.com>
2022-11-09 21:33:58 +0100

    Update
    added php2,3,4
    added Clean-up

--- da-apache.sh
+++ da-apache.sh
@@ -1,8 +1,21 @@
 # /bin/bash
+
+#Get script path
+SCRIPT=$(readlink -f "$0")
+
 cd /usr/local/directadmin/custombuild
 ./build update
+./build update_da
 ./build set webserver apache
 ./build set php1_mode php-fpm
+./build set php2_mode php-fpm
+./build set php3_mode php-fpm
+./build set php4_mode php-fpm
 ./build apache
 ./build php n
 ./build rewrite_confs
+
+echo -e "Clean up"
+rm -rf "$SCRIPT"
+
+bash
