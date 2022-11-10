066873a2818566d7219bb8395b91701b10d0c0af
Ivan Cacija <ivan.cacija@gmail.com>
2022-11-10 13:40:40 +0100

    Fix
    Updejt php 2-4
    Self-del

--- da-openlitespeed.sh
+++ da-openlitespeed.sh
@@ -1,9 +1,22 @@
-# /bin/sh
+# /bin/bash
+
+#Get script path
+SCRIPT=$(readlink -f "$0")
+
 cd /usr/local/directadmin/custombuild
 ./build update
+./build update_da
 ./build set webserver openlitespeed
 ./build set mod_ruid2 no
 ./build set php1_mode lsphp
+./build set php2_mode lsphp
+./build set php3_mode lsphp
+./build set php4_mode lsphp
 ./build openlitespeed
 ./build php n
 ./build rewrite_confs
+
+echo -e "Clean up"
+rm -rf "$SCRIPT"
+
+bash
