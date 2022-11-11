42665b85883f9e85cb109aa7397149a83fa11714
Ivan Cacija <ivan.cacija@gmail.com>
2022-11-11 14:20:05 +0100

    Update user_backup_post.sh

--- directadmin/user_backup_post.sh
+++ directadmin/user_backup_post.sh
@@ -11,7 +11,7 @@ REQUIRED_PATH=/home/$RESELLER/admin_backups
 
 if [ "$BACKUP_PATH" = "$REQUIRED_PATH" ]; then
    if [ "`echo $file | cut -d. -f4,5`" = "tar.zst" ]; then
-       NEW_FILE=`echo $file | cut -d. -f1,2,3`.`date +%F-%H-%M`.tar.zst
+       NEW_FILE=`echo $file | cut -d. -f1,2,3`.`date +%F-%H_%M`.tar.zst
        if [ -s "$file" ] && [ ! -e "$NEW_FILE" ]; then
            mv $file $NEW_FILE
        fi
