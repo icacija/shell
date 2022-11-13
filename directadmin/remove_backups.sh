db7812cd0c5dcc7cf00000e6637b8e7c73127aac
Ivan Cacija <ivan.cacija@gmail.com>
2022-11-13 12:10:29 +0100

    Update remove_backups.sh
    Fixed minior stuff

--- directadmin/remove_backups.sh
+++ directadmin/remove_backups.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-# Delete old backups
-version="1.2"
+version="1.3"
 backup_directory="/home/admin/admin_backups" # No trailing /
 backup_time="2" # Remove backups how many days old? Number variable only!
 
 
 # find $backup_directory -mtime +$backup_time -exec rm -rf {} +
 # find $backup_directory -maxdepth 1 -type f -mtime $backup_time
-find $backup_directory -maxdepth 1 -type f -mtime $backup_time -exec rm -rf {
+find $backup_directory -maxdepth 1 -type f -mtime $backup_time -exec rm -rf {} +
+
+echo -e "Thank you for using Remove_backups $version"
