1d345234b294f1b51cb4b36f22042689c499a215
Ivan Cacija <ivan.cacija@gmail.com>
2024-05-13 14:09:32 +0200

    Update da-login.sh

--- directadmin/da-login.sh
+++ directadmin/da-login.sh
@@ -1,3 +1,4 @@
 # /bin/bash
 
-/usr/local/directadmin/directadmin --create-login-url user=admin
+# /usr/local/directadmin/directadmin --create-login-url user=admin
+/usr/local/directadmin/directadmin create-login-url --user=admin
