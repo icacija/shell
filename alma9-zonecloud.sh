922750040838f7d86eea6e23e632fa51f5814aee
Ivan Cacija <ivan.cacija@gmail.com>
2024-10-14 23:28:59 +0200

    Update alma9-zonecloud.sh

--- alma9-zonecloud.sh
+++ alma9-zonecloud.sh
@@ -3,8 +3,6 @@
 #Get script path
 SCRIPT=$(readlink -f "$0")
 
-wget https://raw.githubusercontent.com/icacija/shell/main/cpanel-cse.sh
-bash cpanel-cse.sh
 wget https://repo.nixpal.com/el9/nixpal-el9-1.0-0.el9.x86_64.rpm
 yum localinstall nixpal-el9-1.0-0.el9.x86_64.rpm
 yum clean all
